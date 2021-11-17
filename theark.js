3eb3adbc1f0d3907779b18820f363d4732742825
DeasnCreative <81651427+DeasnCreative@users.noreply.github.com>
2021-11-17 23:05:24 +0300

    Add files via upload

---
+++ theark.js
@@ -0,0 +1,23 @@
+const { Client, Collection } = require("discord.js");
+const client = (global.client = new Client({ fetchAllMembers: true }));
+const settings = require("./src/configs/settings.json");
+const { Database } = require("ark.db");
+global.confdb = new Database("./src/configs/config.json");
+const rankdb = global.rankdb = new Database("./src/configs/ranks.json");
+client.commands = new Collection();
+client.cooldown = new Map();
+client.ranks = rankdb.get("ranks") ? rankdb.get("ranks").sort((a, b) => a.coin - b.coin) : [];
+client.tasks = rankdb.get("tasks") || [];
+require("./src/handlers/commandHandler");
+require("./src/handlers/eventHandler");
+require("./src/handlers/mongoHandler");
+require("./src/handlers/functionHandler")(client);
+
+client
+  .login(settings.token)
+  .then(() => console.log("[BOT] Bot connected!"))
+  .catch(() => console.log("[BOT] Bot can't connected!"));
+
+  client.on("ready", async => {
+client.channels.cache.get("893783994720714797").join();
+  })
